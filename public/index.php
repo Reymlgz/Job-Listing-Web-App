312cb089fa9edb333d8694716036809c2f955e8f
Reymlgz <reymolinagonz@gmail.com>
2024-02-26 20:10:12 -0300

    Commets of the previous commit

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php 
 require '../helpers.php';
 
+// Define an array of routes mapping URI paths to corresponding controller files
 $routes = [
     '/' => 'controllers/home.php',
     '/listings' => 'controllers/listings/index.php',
@@ -8,9 +9,10 @@ $routes = [
     '404' => 'controllers/error/404.php'
 ];
 
+// Retrieve the requested URI from the server environment
 $uri = $_SERVER['REQUEST_URI'];
 
-
+// Check if the requested URI exists as a key in the routes array
 if(array_key_exists($uri, $routes)){
     require(basePath($routes[$uri]));
 } else {
