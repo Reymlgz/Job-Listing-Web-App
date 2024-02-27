dbcb116e75bb10926ec534a36cd15afc7a259957
Reymlgz <reymolinagonz@gmail.com>
2024-02-27 14:42:37 -0300

    Routes & Roters files created

--- public/index.php
+++ public/index.php
@@ -1,20 +1,10 @@
 <?php 
 require '../helpers.php';
 
-$routes = [
-    '/' => 'controllers/home.php',
-    '/listings' => 'controllers/listings/index.php',
-    '/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php'
-];
 
 $uri = $_SERVER['REQUEST_URI'];
 
+require basePath('router.php');
 
-if(array_key_exists($uri, $routes)){
-    require(basePath($routes[$uri]));
-} else {
-    require basePath($routes['404']);
-}
 
 ?>
